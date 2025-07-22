b235803619bf78c6f5035f7ba8056141a398b005
KwinZainy <gbadamosiblessing651@gmail.com>
2025-07-22 13:55:00 +0100

    initial commit

---
+++ src/pages/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <section>
+            <h1>Z-</h1>
+            <p>tech</p>
+        </section>
+        <section>
+            <p>Home</p>
+            <p>About</p>
+            <p>Skills</p>
+            <p>Projects</p>
+            <p>Contact</p>
+        </section>
+    </div>
+  )
+}
+
+export default Nav
