8dcd6dcede9ce4d546702d1db4d598a0df39b41f
KwinZainy <gbadamosiblessing651@gmail.com>
2025-07-26 21:09:04 +0100

    animated text

--- src/pages/AnimatedText.jsx
+++ src/pages/AnimatedText.jsx
@@ -1,25 +1,25 @@
-// import {TypeAnimation} from 'react-type-animation'
+import {TypeAnimation} from 'react-type-animation'
 
-// const AnimatedText = () => {
-//   return (
-//     <TypeAnimation
-//     sequence ={[
-//       'React Enthusiast.',
-//       2000,
-//       'Web Designer.',
-//       2000,
-//       'Problem Solver.',
-//       2000,
-//       'Code Lover.',
-//       2000,
+const AnimatedText = () => {
+  return (
+    <TypeAnimation
+    sequence ={[
+      'React Enthusiast.',
+      2000,
+      'Web Designer.',
+      2000,
+      'Problem Solver.',
+      2000,
+      'Code Lover.',
+      2000,
       
-//     ]}
-//     wrapper = 'span'
-//     speed = {50}
-//     repeat = {Infinity}
-//     className = 'text-[50px] text-[#074188] font-bold backdrop-blur-md bg-white/70 z-10 italic rounded-md'
-//     />
-//   )
-// }
+    ]}
+    wrapper = 'span'
+    speed = {50}
+    repeat = {Infinity}
+    className = 'text-[50px] text-[#074188] font-bold backdrop-blur-md bg-white/70 z-10 italic rounded-md'
+    />
+  )
+}
 
-// export default AnimatedText
+export default AnimatedText
