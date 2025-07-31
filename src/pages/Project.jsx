da3b1839ada48e912384a4840cf19f327e836c81
KwinZainy <gbadamosiblessing651@gmail.com>
2025-07-31 10:44:25 +0100

    link

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -10,31 +10,31 @@ const Project = () => {
             <div className='bg-[#F2F7F8] rounded-3xl shadow-2xl px-16 py-12 max-tablet:mb-6 max-mobile:mb-6 pt-8 transition duration-300 hover:scale-105'>
                <a href="https://piggyvest-with-react-wp53.vercel.app/" target='_blank'  rel=' noopener noreferrer' >
                 <img src="./piggy.jpg" alt="" className=' w-[270px] rounded-[80%] border-black border-2 '/>
-                <p className=' text-center pt-3 text-[20px] text-[#074188] font-medium'>Cloned Website</p>
+                <p className=' text-center pt-3 text-[20px] text-[#074188] font-medium'>Piggyvest Clone</p>
                 </a> 
             </div>
             <div className='bg-[#F2F7F8] p-14 rounded-3xl shadow-2xl transition max-tablet:mb-6 duration-300 max-mobile:mb-6 hover:scale-105 '>
-                <a href=" https://bad-git-main-blessings-projects-70e43eb1.vercel.app/" target=' _blank' rel=' noopener nereferrer'>
+                <a href="https://bad-pied.vercel.app/ " target=' _blank' rel=' noopener nereferrer'>
                 <img src="./bad.jpg" alt="" className=' w-[300px] rounded-[80%] border-black border-2 transition' />
                 <p className=' text-center pt-3 text-[20px] text-[#074188] font-medium'>Food Website</p>
                 </a>
                 </div>
             <div className='bg-[#F2F7F8] p-14 rounded-3xl shadow-2xl transition max-tablet:mb-6 duration-300 max-mobile:mb-6 hover:scale-105 '>
-                <a href=" https://countries-git-main-blessings-projects-70e43eb1.vercel.app/" target='_blank' rel=' noopener noreferrer'>
+                <a href=" countries-liard-one.vercel.app" target='_blank' rel=' noopener noreferrer'>
                 <img src="./country.jpg" alt="" className=' w-[300px] rounded-[80%] border-black border-2 transition'/>
                 <p className=' text-center pt-3 text-[20px] text-[#074188] font-medium'>All Countries</p>
                 </a>
             </div>
             <div className='bg-[#F2F7F8] p-14 rounded-3xl shadow-2xl mt-8 transition max-tablet:mb-6 duration-300 max-mobile:mb-6 hover:scale-105 '>
-                <a href=" https://calculator-b-git-main-blessings-projects-70e43eb1.vercel.app//" target=' _blank' rel=' noopener noreferrer'>
-                <img src="./calculator.jpg" alt="" className=' w-[300px] rounded-[80%] border-black border-2 transition'/>
-                <p className=' text-center pt-3 text-[20px] text-[#074188] font-medium'>Calculator</p>
+                <a href="https://second-cloned-website-meta.vercel.app/" target=' _blank' rel=' noopener noreferrer'>
+                <img src="./meta.jpg" alt="" className=' w-[300px] rounded-[80%] border-black border-2 transition'/>
+                <p className=' text-center pt-3 text-[20px] text-[#074188] font-medium'>Meta Clone</p>
                 </a>
             </div>
             <div className='bg-[#F2F7F8] shadow-2xl p-14 mt-8 rounded-3xl transition max-tablet:mb-6 duration-300 max-mobile:mb-6 hover:scale-105'>
-                <a href="https://form-gpnv-git-main-blessings-projects-70e43eb1.vercel.app/" target='_blank' rel=' noopener noreferrer'>
-                <img src="./form.jpg" alt="" className=' w-[300px] rounded-[80%] border-black border-2 transition'/>
-                <p className=' text-center pt-3 text-[20px] text-[#074188] font-medium'>A Tag Form</p>
+                <a href="https://moove-nu.vercel.app/" target='_blank' rel=' noopener noreferrer'>
+                <img src="./moove.jpg" alt="" className=' w-[300px] rounded-[80%] border-black border-2 transition'/>
+                <p className=' text-center pt-3 text-[20px] text-[#074188] font-medium'>Moove Clone</p>
                 </a>
             </div>
         </section>
